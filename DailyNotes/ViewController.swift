5c7bba986908ce8cc52a9b9c79dadad310684f99
NRintaro <hemisphere.babylon@icloud.com>
2018-10-22 18:44:11 +0900

    Initial Commit

---
+++ DailyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DailyNotes
+//
+//  Created by 中村麟太郎 on 2018/10/22.
+//  Copyright © 2018年 中村麟太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
